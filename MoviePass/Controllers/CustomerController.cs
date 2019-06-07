9cd9bb0ca5ee1d565f2963b2c1d983f5d87f70fd
sikin <sikin@DESKTOP-SVMGGQM>
2019-06-06 22:51:25 -0400

    customers added edited updated

--- MoviePass/Controllers/CustomerController.cs
+++ MoviePass/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using MoviePass.Models;
 using System.Data.Entity;
+using MoviePass.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,37 @@ namespace MoviePass.Controllers
             return View(customersList);
         }
 
+       public ActionResult New()
+        {
+            var membersList = _context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                MembershipTypes= membersList
+            };
+
+            return View("CustomerForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customers)
+        {
+            if(customers.Id==0)
+            {
+                _context.Customers.Add(customers);
+
+            }
+            else
+            {
+                var customerdb = _context.Customers.Single(c => c.Id == customers.Id);
+                customerdb.Name = customers.Name;
+                customerdb.Birthdate = customers.Birthdate;
+                customerdb.MembershipType = customers.MembershipType;
+                customerdb.IsSubscribedToNewsLetter = customers.IsSubscribedToNewsLetter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
         public ActionResult Details(int id)
         {
             var customersList = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c=> c.Id==id);
@@ -39,5 +71,24 @@ namespace MoviePass.Controllers
             }
             return View(customersList);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var selectedCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (selectedCustomer == null)
+            {
+                HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customers = selectedCustomer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+
     }
 }
---
+++ MoviePass/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MoviePass.Models;
+
+namespace MoviePass.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customers { get; set; }
+    }
+}
